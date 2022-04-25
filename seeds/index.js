4da7188bfd87f89b7886a470014f45c2b46adf14
TrIliadis <triliadis@hotmail.com>
2022-04-26 00:39:40 +0300

    show page

--- seeds/index.js
+++ seeds/index.js
@@ -24,12 +24,17 @@ function randomDate(start, end) {
 //Dummy seed DB
 const seedDB = async () => {
   await Course.deleteMany({});
-  for (let i = 0; i < 25; i++) {
-    //get random images from
+  for (let i = 0; i < 8; i++) {
+    //get random images
     const img = await axios.get(
       "https://api.unsplash.com/photos/random/?client_id=5oyzn1pALQH16hRegJFZsFnTV8Ov9LqrYGnm5NYYPhI&collections=402504"
     );
-    console.log(img);
+    const img2 = await axios.get(
+      "https://api.unsplash.com/photos/random/?client_id=5oyzn1pALQH16hRegJFZsFnTV8Ov9LqrYGnm5NYYPhI&collections=606028"
+    );
+    const img3 = await axios.get(
+      "https://api.unsplash.com/photos/random/?client_id=5oyzn1pALQH16hRegJFZsFnTV8Ov9LqrYGnm5NYYPhI&collections=907185"
+    );
     const course = new Course({
       title: courses[i].title,
       description: courses[i].description,
@@ -37,7 +42,13 @@ const seedDB = async () => {
       lastActive: new Date().toLocaleDateString(),
       images: [
         {
-          url: img.data.urls.regular,
+          url: img.data.urls.raw + "&w=1500&dpr=2&h=500",
+        },
+        {
+          url: img2.data.urls.raw + "&w=1500&dpr=2&h=500",
+        },
+        {
+          url: img3.data.urls.raw + "&w=1500&dpr=2&h=500",
         },
       ],
     });
