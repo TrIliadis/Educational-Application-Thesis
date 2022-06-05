d8fc1831ab5db625ca0d8bf16bb165822d2ced0f
TrIliadis <triliadis@hotmail.com>
2022-06-05 22:17:47 +0300

    contact form feedback

---
+++ public/javascripts/register.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("registerForm");
+const button = document.getElementById("registerButton");
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const surname = document.getElementById("surname");
+const password = document.getElementById("password");
+const validatePassword = document.getElementById("validatePassword");
